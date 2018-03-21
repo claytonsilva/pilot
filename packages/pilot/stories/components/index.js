2ea77f40f81004d9ec10f0d89d8bc9f9365da677
uselessdev <wallacebatistaoliveira@gmail.com>
2018-03-21 14:32:28 -0300

    transactions: add details card component

--- packages/pilot/stories/components/index.js
+++ packages/pilot/stories/components/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
+import Section from './Section'
+import TransactionDetailsCard from './TransactionDetailsCard'
+
 storiesOf('Components', module)
-  .add('components', () => (
-    <span>Components</span>
+  .add('TransactionDetailsCard', () => (
+    <Section>
+      <TransactionDetailsCard />
+    </Section>
   ))
